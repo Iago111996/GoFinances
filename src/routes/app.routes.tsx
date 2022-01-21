0f3d116191b4597a90255c7b403559493978557d
Iago111996 <iago111996@gmail.com>
2022-01-21 10:18:51 -0300

    add page table-flatlist

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,22 +1,25 @@
-import * as React from 'react';
-import { Platform } from 'react-native';
-import { useTheme } from 'styled-components';
-import { MaterialIcons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from "react";
+import { Platform } from "react-native";
+import { useTheme } from "styled-components";
+import { MaterialIcons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { Dashboard } from '../screens/Dashboard';
-import { Register } from  '../screens/Register';
-import { Resume } from '../screens/Resume';
-import { SignIn } from '../screens/SignIn';
+import { Dashboard } from "../screens/Dashboard";
+import { Register } from "../screens/Register";
+import { Resume } from "../screens/Resume";
+import { SignIn } from "../screens/SignIn";
+import { TabTop } from "../screens/TabTop";
 
 export type RootBottomTabParamList = {
   Listagem: undefined;
   Cadastrar: undefined;
   Resumo: undefined;
   Splash: undefined;
+  TabTop: undefined;
 };
 
-const { Navigator, Screen } = createBottomTabNavigator<RootBottomTabParamList>();
+const { Navigator, Screen } =
+  createBottomTabNavigator<RootBottomTabParamList>();
 
 interface AppRoutesProps {}
 
@@ -25,74 +28,71 @@ export const AppRoutes = ({}: AppRoutesProps) => {
 
   return (
     <Navigator
-    initialRouteName="Cadastrar"
-    screenOptions={{
-      tabBarActiveTintColor: theme.colors.secondary,
-      tabBarInactiveTintColor: theme.colors.text,
-      tabBarLabelPosition: 'beside-icon',
-      tabBarStyle: {
-        paddingVertical: Platform.OS === 'ios' ? 20 : 0,
-        height: 88,
-      },
-      headerShown: false,
-    }}
+      initialRouteName="Cadastrar"
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: "beside-icon",
+        tabBarStyle: {
+          paddingVertical: Platform.OS === "ios" ? 20 : 0,
+          height: 88,
+        },
+        headerShown: false,
+      }}
     >
-      <Screen 
+      <Screen
         name="Listagem"
         component={Dashboard}
-        options= {{
-          tabBarIcon: (({ size, color}) => (
+        options={{
+          tabBarIcon: ({ size, color }) => (
             <MaterialIcons
               name="format-list-bulleted"
               size={size}
               color={color}
             />
-          )),
+          ),
         }}
       />
 
-      <Screen 
+      <Screen
         name="Cadastrar"
         component={Register}
-        options= {{
-          tabBarIcon: (({ size, color}) => (
-            <MaterialIcons
-              name="attach-money"
-              size={size}
-              color={color}
-            />
-          )),
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
         }}
       />
 
-      <Screen 
+      <Screen
         name="Resumo"
         component={Resume}
-        options= {{
-          tabBarIcon: (({ size, color}) => (
-            <MaterialIcons
-              name="pie-chart"
-              size={size}
-              color={color}
-            />
-          )),
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
         }}
       />
 
-      <Screen 
+      <Screen
         name="Splash"
         component={SignIn}
-        options= {{
-          tabBarIcon: (({ size, color}) => (
-            <MaterialIcons
-              name="attach-money"
-              size={size}
-              color={color}
-            />
-          )),
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="TabTop"
+        component={TabTop}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
         }}
       />
     </Navigator>
   );
 };
-
--- src/screens/TabTop/index.tsx
+++ src/screens/TabTop/index.tsx
@@ -1,20 +1,229 @@
-import React from 'react';
-import { AppRoutes } from '../../routes/app.routes';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  createRef,
+  forwardRef,
+} from "react";
+import { useTheme } from "styled-components";
 
+import { StatusBar } from "expo-status-bar";
 import {
-  Container,
-  Content,
-  Title
-} from './styles';
+  StyleSheet,
+  findNodeHandle,
+  Text,
+  View,
+  Dimensions,
+  FlatList,
+  Animated,
+  Image,
+  Modal,
+} from "react-native";
+
+import { Container } from "./styles";
+
+const images = {
+  man: "https://images.pexels.com/photos/3147528/pexels-photo-3147528.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  women:
+    "https://images.pexels.com/photos/2552130/pexels-photo-2552130.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  kids: "https://images.pexels.com/photos/5080167/pexels-photo-5080167.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  skullcandy:
+    "https://images.pexels.com/photos/5602879/pexels-photo-5602879.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  help: "https://images.pexels.com/photos/2552130/pexels-photo-2552130.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+};
+
+interface DataProps {
+  key: string;
+  title: string;
+  image: string;
+  ref: any;
+}
+
+const itemMap = images as Record<string, any>;
+const data: DataProps[] = Object.keys(images).map((i) => ({
+  key: i,
+  title: i,
+  image: itemMap[i],
+  ref: createRef(),
+}));
+const { width, height } = Dimensions.get("screen");
+
+interface TabProps {
+  item: DataProps;
+  ref: any;
+}
+const Tab = forwardRef(({ item }: TabProps, ref) => {
+  return (
+    <View ref={ref as React.LegacyRef<View>}>
+      <Text
+        style={{
+          color: "white",
+          fontSize: 84 / data.length,
+          fontWeight: "800",
+          textTransform: "uppercase",
+        }}
+      >
+        {item.title}
+      </Text>
+    </View>
+  );
+});
+
+interface IndicatorProps {
+  scrollX: Animated.Value;
+  measures: MeasureProps[];
+}
+
+function Indicator({ measures, scrollX }: IndicatorProps) {
+  const inputRange = data.map((_, i) => i * width);
+
+  const indicatorWidth = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure) => measure.width),
+  });
+
+  const translateX = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure) => measure.x),
+  });
+
+  return (
+    <Animated.View
+      style={{
+        position: "absolute",
+        height: 4,
+        width: indicatorWidth,
+        backgroundColor: "orange",
+        left: 0,
+        bottom: -8,
+        transform: [{ translateX }],
+      }}
+    />
+  );
+}
+
+function IndicatorFlex() {
+  return (
+    <View
+      style={{
+        position: "absolute",
+        height: 4,
+        width: width,
+        backgroundColor: "transparent",
+        left: 0,
+        bottom: -8,
+      }}
+    />
+  );
+}
+
+interface TabsProps {
+  scrollX: Animated.Value;
+  data: DataProps[];
+}
+
+interface MeasureProps {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+function Tabs({ scrollX, data }: TabsProps) {
+  const [measures, setMeasures] = useState<MeasureProps[]>([]);
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    let m: MeasureProps[] = [];
+    data.forEach((item) => {
+      item.ref.current.measureLayout(
+        containerRef.current,
+        (x: number, y: number, width: number, height: number) => {
+          m.push({
+            x,
+            y,
+            width,
+            height,
+          });
+
+          if (m.length === data.length) {
+            setMeasures(m);
+          }
+        }
+      );
+    });
+  }, []);
+
+  return (
+    <View style={{ position: "absolute", top: 50 }}>
+      <View
+        ref={containerRef}
+        style={{ flexDirection: "row", justifyContent: "space-evenly", width }}
+      >
+        {data.map((item) => {
+          return <Tab key={item.key} item={item} ref={item.ref} />;
+        })}
+      </View>
+
+      <IndicatorFlex />
+      {measures.length > 0 && (
+        <Indicator measures={measures} scrollX={scrollX} />
+      )}
+    </View>
+  );
+}
 
 export function TabTop() {
-  return(
-    <Container>
-        <AppRoutes />
+  const scrolX = useRef(new Animated.Value(0)).current;
 
-        <Content>
-            <Title>oioioioio</Title>
-        </Content>
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <View style={styles.container}>
+        <StatusBar hidden />
+        <Animated.FlatList
+          data={data}
+          keyExtractor={(item) => String(item.key)}
+          horizontal
+          pagingEnabled
+          bounces={false}
+          showsHorizontalScrollIndicator={false}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrolX } } }],
+            { useNativeDriver: false }
+          )}
+          renderItem={({ item }) => {
+            return (
+              <View style={{ width, height }}>
+                <Image
+                  source={{ uri: item.image }}
+                  style={{ flex: 1, resizeMode: "cover" }}
+                />
+
+                <View
+                  style={[
+                    StyleSheet.absoluteFillObject,
+                    { backgroundColor: "rgba(0,0,0,0.3)" },
+                  ]}
+                />
+              </View>
+            );
+          }}
+        />
+
+        <Tabs scrollX={scrolX} data={data} />
+      </View>
     </Container>
-);
-};
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
